e4b21020b699a5c71a2d35ad97782fa27e8b9ecf
jgohbb <55856950+jgohbb@users.noreply.github.com>
2019-11-16 00:26:15 -0700

    on track

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,58 +7,70 @@
 // Functions
 // Main process
 
+var score = 0;
+var currentQuestion = 0;
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var noAnswer = 0;
+var timer = 20;
+var questionsList = [
+    {
+        title: "What is the King of fruit?",
+        options: ["pineapple", "mango", "dragon fruit", "durian"],
+        answer: 3,
+        image: "<img class='center-block img-right src='../images/durian.jpg'>",
+    },
+    {
+        title: "Is fortune cookie invented?",
+        options: ["Hong Kong", "Canada", "USA", "Beijing"],
+        answer: 2,
+        image: "<img class='center-block img-right src='../images/fortunecookie.jpg'>",
+    },
+    {
+        title: "What is an Aussie barbie?",
+        options: ["doll", "barbeque", "pub", "shrimp"],
+        answer: 1,
+        image: "<img class='center-block img-right src='../images/aussiebbq.jpg'>",
+    },
+    {
+        title: "which chef has the most Michelin stars?",
+        options: ["Gordon Ramsey", "Anne-Sophie Pic", "Joel Robuichon", "Alain Ducasse"],
+        answer: 2,
+        image: "<img class='center-block img-right src='../images/joel_robuchon.png'>",
+    }
+];
+
 $(document).ready(function() {
 
-    var questionList = [
-        "What is the King of fruit?",
-        "Is fortune cookie invented?",
-        "What is an Aussie barbie?",
-        "which chef has the most Michelin stars?"
-    ];
-    var optionList = [
-        ["pineapple", "mango", "dragon fruit", "durian"],
-        ["Hong Kong", "Canada", "USA", "Beijing"],
-        ["doll", "barbeque", "pub", "shrimp"],
-        ["Gordon Ramsey", "Anne-Sophie Pic", "Joel Robuichon", "Alain Ducasse"]
-    ]
-    var answerList = [
-        "durian",
-        "USA",
-        "barbeque",
-        "Joel Robuichon",        
-    ]; 
-    var imageList = [
-        "<img class='center-block img-right src='../images/durian.jpg'>", 
-        "<img class='center-block img-right src='../images/fortunecookie.jpg'>", 
-        "<img class='center-block img-right src='../images/aussiebbq.jpg'>",
-        "<img class='center-block img-right src='../images/joel_robuchon.png'>",
-    ];
-    var correctAnswer   = 0;
-    var wrongAnswer     = 0;
-    var noAnswer        = 0;
-    var remainingTime   = 20;
-
-
-    function introScreen() {
-        initiateScreen = "<p class='text center main-button-container'><a class='btn btn-lg btn-block start-button' href='assets/images/startbutton.jpg'>START QUIZ</a></p>";
-        $(".questions").html(initiateScreen);
-        //console.log(initiateScreen);
-    }
-    
-    introScreen();
-
-    $("form").on("click", ".start-button", function(event) {
-        event.preventDefault();
-        $('.start-pg').empty()
-        createHTML();
+    $('.start a').click(function(e){
+        e.preventDefault();
+        $('.start').hide();
+        $('.wrapper').show();
+        showQuestion();
     });
 
-    function createHTML() {
-        gamePage = 
+    
+
+
+
+
+    function showQuestions() {
+        var question = questionsList[currentQuestion];
+        $('.Q&A h3').text(question.title);
+        $('.Q&A p').html('');
+        for(var i = 0; i < question.options.length; i++) {
+            $('.Q&A');
+        }
     };
 
+    
 
-}
+    funtion resetState() {
+
+
+    }
+
+});
 
 
 
