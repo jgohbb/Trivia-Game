485935e3b6fad6fbc0364a0bd24cbc86021bfced
jgohbb <55856950+jgohbb@users.noreply.github.com>
2019-11-16 01:39:27 -0700

    making good progress with improvements

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,14 @@
 // Functions
 // Main process
 
+//create Global variables
 var score = 0;
 var currentQuestion = 0;
 var correctAnswer = 0;
 var wrongAnswer = 0;
 var noAnswer = 0;
 var timer = 20;
-var questionsList = [
+var questionList = [
     {
         title: "What is the King of fruit?",
         options: ["pineapple", "mango", "dragon fruit", "durian"],
@@ -37,7 +38,7 @@ var questionsList = [
         options: ["Gordon Ramsey", "Anne-Sophie Pic", "Joel Robuichon", "Alain Ducasse"],
         answer: 2,
         image: "<img class='center-block img-right src='../images/joel_robuchon.png'>",
-    }
+    },
 ];
 
 $(document).ready(function() {
@@ -45,42 +46,31 @@ $(document).ready(function() {
     $('.start a').click(function(e){
         e.preventDefault();
         $('.start').hide();
+        $('.summary').hide();
         $('.wrapper').show();
         showQuestion();
+
+    });
+
+    //act like a radio button with highlight
+    $('.quiz ul').on('click', 'li', function() {
+        $('.selected').removeClass('selected');
+        $(this).addClass('selected');
     });
 
     
 
-
-
-
-    function showQuestions() {
-        var question = questionsList[currentQuestion];
-        $('.Q&A h3').text(question.title);
-        $('.Q&A p').html('');
-        for(var i = 0; i < question.options.length; i++) {
-            $('.Q&A');
-        }
-    };
-
-    
-
-    funtion resetState() {
-
-
-    }
-
 });
 
 
-
-
-
-
-
-
-
-
-
-
-)
+function showQuestion() {
+    var question = questionList[currentQuestion];
+    $('.quiz h4').text(question.title);
+    $('.quiz ul').html('');
+    for(var i = 0; i < question.options.length; i++) {
+        $('.quiz ul').append("<li id='"+i+"'>" + question.options[i] + "</li>");
+    
+    }
+};
+//funtion resetState() {
+//}
